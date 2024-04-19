e7c46c99f0bff688968e7ac3d12cdc5a7f2b963f
Munene-Murithi <sammymunene22@gmail.com>
2024-04-20 02:00:44 +0300

    added data types

---
+++ data types/main.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Integers
+  int age = 25; // Example of an integer data type
+  print('Age: $age');
+
+  // Doubles
+  double height = 5.8; // Example of a double data type
+  print('Height: $height feet');
+
+  // Strings
+  String name = 'Sammy Murithi'; // Example of a string data type
+  print('Name: $name');
+
+  // Lists
+  List<String> fruits = ['Apple', 'Banana', 'Orange']; // Example of a list data type
+  print('Fruits: $fruits');
+
+  // Maps
+  Map<String, dynamic> person = {
+    'name': 'Alice',
+    'age': 30,
+    'city': 'New York'
+  }; // Example of a map data type
+  print('Person: $person');
+}
